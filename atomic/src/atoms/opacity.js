ad7f93ce9f454574b164ccc679100fb431a9cb70
Kilian McMahon <kil@kilmc.com>
2017-06-10 13:24:57 -0400

    Add watch functionality

--- atomic/src/atoms/opacity.js
+++ atomic/src/atoms/opacity.js
@@ -3,6 +3,6 @@ import {
 } from '../../atomic.config.js';
 
 export const opacity = {
-  props: { 'o': 'opacity' },
+  props: { 'oppp': 'opacity' },
   values: opacities([0, 1, 3, 6, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 }
